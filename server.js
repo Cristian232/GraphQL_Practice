38dd0cf462bff684fe2d2f8fbbc2cee2cadd19df
Cristian <Cristi@noemail.com>
2023-07-06 23:51:20 +0300

    Added dummy gl schema and query

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express')
-const {graphqlHTTP} = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
 
 const app = express();
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "Hello",
+        fields: ()=>({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hellow :)'
+            }
+        })
+    })
+})
+
 app.use('/graphql', graphqlHTTP({
-    graphiql: true
+    graphiql: true , schema: schema
 }))
 
-app.listen(5000, ()=> console.log("Server Running"))
+app.listen(5000, () => console.log("Server Running"))
